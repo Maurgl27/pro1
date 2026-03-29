a4f0fdba88286013123648e4db5a530c24b85922
Maurgl27 <146876989+Maurgl27@users.noreply.github.com>
2026-03-28 22:02:59 -0300

    gitday

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
-
+//solo para llenar mi github subo este commit xd
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <StrictMode>
